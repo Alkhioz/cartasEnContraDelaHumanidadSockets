04cc26d2df5d5f6dd04e52c156357ec23e1e1fbb
Alejandro Mendoza <33111241+Alkhioz@users.noreply.github.com>
2022-10-24 21:43:01 -0500

    added json server

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express= require('express')
+const jsonServer = require('json-server')
 const http = require('http')
 const path = require('path')
 
 const app = express()
 app.set("port", 8081)
-const server=http.createServer(app)
+
+app.use('/api', jsonServer.router('database/db.json'))
 app.use(express.static(path.join(__dirname,"public")))
 
+const server=http.createServer(app)
 server.listen(process.env.PORT || app.get('port'),()=>{
     console.log("server on port ",app.get('port'))
 })
